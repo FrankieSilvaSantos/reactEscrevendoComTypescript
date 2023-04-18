53fdf5dd33f3e9ffda25a6d41f682ea3ab36e09d
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2023-04-18 15:23:55 -0300

    atualizado

---
+++ src/components/botao/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Botao extends React.Component {
+    render() {
+        const color = 'red';
+        return (
+            <button style={{backgroundColor: color}}>
+                Botão
+            </button>
+        )
+    }
+
+}
+
+export default Botao;
