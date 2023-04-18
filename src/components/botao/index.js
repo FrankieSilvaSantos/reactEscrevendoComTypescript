ebe04729f515e2b6ca826ed59d853359dc96664f
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2023-04-18 16:07:34 -0300

    atualizei

--- src/components/botao/index.js
+++ src/components/botao/index.js
@@ -2,9 +2,12 @@ import React from "react";
 
 class Botao extends React.Component {
     render() {
-        const color = 'red';
+        const estaAtivo = true;
+        const styles = {
+            backgroundColor: estaAtivo ? "green" : "red"
+        }
         return (
-            <button style={{backgroundColor: color}}>
+            <button style={styles}>
                 Botão
             </button>
         )
